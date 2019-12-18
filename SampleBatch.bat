dd92e833fb9e0119b09506119aae46349129cf65
Ishan Shinde <ishanshinde17@gmail.com>
2019-12-18 15:36:20 -0800

    Deleting unused files

--- SampleBatch.bat
+++ SampleBatch.bat
@@ -1,7 +1,4 @@
 @echo off
 start python ConnectToDB.py -g 50072 -s "prf01cavsmtp01" -q 1
-pause
 start python ConnectToDB.py -g 50073 -s "prf01cavsmtp01" -q 1
-pause
 start python ConnectToDB.py -g 50074 -s "prf01cavsmtp01" -q 1
-pause
--- test.py
+++
@@ -1,6 +0,0 @@
-import time
-
-for i in range(5):
-    print("Hello There")
-
-time.sleep(5)
